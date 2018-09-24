39bd53772b63d3a26f62697d8c05f60680435171
bwyfed <36172659@qq.com>
2018-09-24 17:54:48 +0800

    改造回调函数调用方式，使用异步函数方式来完成异步操作

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const chalk = require('chalk');
 const path = require('path');
 const conf = require('./config/defaultConfig');
-const route = require('./helper/route')
+const route = require('./helper/route');
 
 const server = http.createServer((req, res) => {
     const filePath = path.join(conf.root, req.url);
