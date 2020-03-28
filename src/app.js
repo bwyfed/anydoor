c9a7feba1feb6a2acd36d7f09c2a40206566f36b
Bianwy <bianwy025@qq.com>
2020-03-28 20:59:34 +0800

    '修改eslint关于缩进规则的配置。'

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const conf = require('./config/defaultConfig');
 const route = require('./helper/route');
 
 const server = http.createServer((req, res) => {
-    const filePath = path.join(conf.root, req.url);
-    route(req, res, filePath);
+  const filePath = path.join(conf.root, req.url);
+  route(req, res, filePath);
 });
 server.listen(conf.port, conf.hostname, () => {
-    const addr = `http://${conf.hostname}:${conf.port}`;
-    console.info(`Server started at ${chalk.green(addr)}`);
+  const addr = `http://${conf.hostname}:${conf.port}`;
+  console.info(`Server started at ${chalk.green(addr)}`);
 });
