687a8c88dd88c6ca01608c020c43de0ea6884cb4
Bianwy <bianwy025@qq.com>
2020-03-28 12:15:24 +0800

    '更新eslint相关的配置。'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,33 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "mocha": true
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "script"
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
     },
-    "rules": {
-        "no-console": ["error", {
-            "allow": ["warn", "error", "info"]
-        }],
-        "indent": [
-            "error",
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': [
+            'error',
             4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ]
     }
 };
--- src/app.js
+++ src/app.js
@@ -7,9 +7,7 @@ const route = require('./helper/route');
 const server = http.createServer((req, res) => {
     const filePath = path.join(conf.root, req.url);
     route(req, res, filePath);
-    
 });
-
 server.listen(conf.port, conf.hostname, () => {
     const addr = `http://${conf.hostname}:${conf.port}`;
     console.info(`Server started at ${chalk.green(addr)}`);
