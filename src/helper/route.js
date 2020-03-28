e14829b99b3b2d734db2c5b71602a30abcaf4918
Bianwy <bianwy025@qq.com>
2020-03-28 20:47:44 +0800

    '实现文件压缩功能。'

--- src/helper/route.js
+++ src/helper/route.js
@@ -6,41 +6,47 @@ const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 const config = require('../config/defaultConfig');
 const mime = require('./mime');
+const compress = require('./compress');
 // 使用模板引擎
 const tplPath = path.join(__dirname, '../template/dir.tpl');
 const source = fs.readFileSync(tplPath, 'utf-8');
 const template = Handlebars.compile(source);
 
 module.exports = async function(req, res, filePath) {
-    try {
-        const stats = await stat(filePath);
-        if (stats.isFile()) {
-            const contentType = mime(filePath);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', contentType);
-            fs.createReadStream(filePath).pipe(res);
-        } else if (stats.isDirectory()) {
-            const files = await readdir(filePath);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            const dir = path.relative(config.root, filePath);
-            const data = {
-                title: path.basename(filePath),
-                dir: dir ? `/${dir}` : '',
-                files: files.map(file => {
-                    return {
-                        file,
-                        icon: mime(file)
-                    };
-                })
-            };
-            res.end(template(data));
-        }
-    } catch (ex) {
-        console.error(ex);
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`${filePath} is not a directory or file\n ${ex.toString()}`);
-        return;
+  try {
+    const stats = await stat(filePath);
+    if (stats.isFile()) {
+      const contentType = mime(filePath);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', contentType);
+      let rs = fs.createReadStream(filePath);
+      if (filePath.match(config.compress)) {
+        // 符合特定文件名的，才进行压缩
+        rs = compress(rs, req, res);
+      }
+      rs.pipe(res);
+    } else if (stats.isDirectory()) {
+      const files = await readdir(filePath);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      const dir = path.relative(config.root, filePath);
+      const data = {
+        title: path.basename(filePath),
+        dir: dir ? `/${dir}` : '',
+        files: files.map(file => {
+          return {
+            file,
+            icon: mime(file)
+          };
+        })
+      };
+      res.end(template(data));
     }
+  } catch (ex) {
+    console.error(ex);
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`${filePath} is not a directory or file\n ${ex.toString()}`);
+    return;
+  }
 };
